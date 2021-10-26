00f0c25916d4f8240874705e807946a394a59d72
Vishal Shankar <vishal119.sh14@gmail.com>
2021-10-26 15:05:37 +0530

    Basket Push

---
+++ src/Services/Catalog/Catalog-API/Data/ICatalogContext.cs
@@ -0,0 +1,14 @@
+﻿using Catalog_API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog_API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products {get;}
+    }
+}
